107c65674bbe6079f47fb374247d609af93c15f6
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-10-05 10:53:14 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,27 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+  ownerAbilities: string[];
+  customerAbilities: string[];
+  getQuoteUrl: string;
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Restaurant Owner'],
+  customerRoles: ['Guest'],
+  tenantRoles: ['Restaurant Owner', 'Restaurant Manager', 'Wait Staff', 'Chef'],
+  tenantName: 'Restaurant',
+  applicationName: 'Restaurant booking engine',
+  addOns: ['file upload', 'chat', 'notifications', 'file'],
+  customerAbilities: ['View restaurant information', 'View menus', 'View table availability', 'Make a reservation'],
+  ownerAbilities: [
+    'Manage restaurant information',
+    'Manage restaurant menus',
+    'Manage restaurant bookings',
+    'Manage restaurant tables',
+  ],
+  getQuoteUrl: 'https://roq-wizzard-git-qa03-roqtech.vercel.app/proposal/771b0181-79f2-45cb-80fe-5b1bb721a470',
+};
